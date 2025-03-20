7ffb354a5d2a1388c233a1e148c9d88cede541e5
ankitsharma93154 <ankitsharma93154@gmail.com>
2025-03-20 23:25:10 +0530

    serverless

--- index.js
+++ index.js
@@ -56,6 +56,7 @@ app.get("/api/articles/:slug", async (req, res) => {
 });
 
 // Start Server
-app.listen(PORT, () => {
-  console.log(` Server running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(` Server running on http://localhost:${PORT}`);
+// });
+export default app;
